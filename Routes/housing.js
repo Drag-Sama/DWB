d957a5f37b75fa67723c98b9678a18c4772ff020
Anto Kajda <antoinekajda@gmail.com>
2026-03-17 15:43:11 +0100

    Logement/Location to Housing

---
+++ Routes/housing.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const housingController = require("../Controller/HousingController")
+
+const router = express.Router();
+
+//Get
+router.get("/", (req,res) => res.send(housingController.fetchHousings()))
+router.get("/:id", (req,res) => res.send(housingController.fetchHousingById(req.params.id)))
+router.get("/filter/:property/:value", (req,res) => res.send(housingController.fetchHousingBy(req.params.property,req.params.value)))
+router.get("/sort/:property", (req,res) => res.send(housingController.fetchHousingSortedBy(req.params.property)))
+
+//Post
+router.post("/:housing", (req,res) => res.send(housingController.createHousing(req.params.housing)))
+//Delete
+router.delete("/:id", (req, res) => res.send(housingController.deleteHousing(req.params.id)))
+//Put
+router.put("/:id/:housing", (req, res) => res.send(housingController.editHousing(req.params.housing, req.params.id)))
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const locationRoute = require('./Routes/location');
+const housingRoute = require('./Routes/housing');
 
 //Initialisation
 const express = require('express')
 const app = express()
 
 
-app.use("/location", locationRoute)
+app.use("/housing", housingRoute)
 app.listen(3000, () => console.log('api_test listening on port 3000!'))
