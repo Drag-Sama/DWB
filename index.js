858b6b53c53549297992d844bbb64de5572c9e9f
HOMIES-HUB <qv62100@gmail.com>
2026-03-30 15:25:38 +0200

    update partie user

--- index.js
+++ index.js
@@ -1,30 +1,20 @@
 const express = require('express')
 const {PrismaClient}  = require("@prisma/client");
 const housingRouter = require("./Routes/housing");
+const userRouter = require("./Routes/housing");
 
 const prisma = new PrismaClient();
 
 const app = express();
 const port = 8080;
 
-async function main() {
-  app.use(express.json());
+app.use(express.json());
 
-  // Register API routes
-  app.use("/housing", housingRouter);
+// Register API routes
+app.use("/housing", housingRouter);
+app.use("/users", userRouter)
 
 
-  app.listen(port, () => {
-    console.log(`Server is listening on port ${port}`);
-  });
-}
-
-main()
-  .then(async () => {
-    await prisma.$connect();
-  })
-  .catch(async (e) => {
-    console.error(e);
-    await prisma.$disconnect();
-    process.exit(1);
-  });
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
