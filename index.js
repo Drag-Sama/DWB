082037fea0e04c66521692f82f205a7e2dbf6fd6
Anto Kajda <antoinekajda@gmail.com>
2026-03-30 17:28:53 +0200

    Housing CRUD

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const housingRouter = require("./Routes/housing")
 
 const app = express()
 
+app.use(express.json())
+
 app.use("/housing", housingRouter);
 
-app.use(express.json())
 app.listen(3000, () => console.log('Server running on port 3000'))
