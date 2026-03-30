4c37d6aa604c2e27a48c4c06d34150c5602a431a
Anto Kajda <antoinekajda@gmail.com>
2026-03-30 15:57:50 +0200

    GetHousings

--- Controller/HousingController.js
+++ Controller/HousingController.js
@@ -4,10 +4,24 @@ const prisma = new PrismaClient()
 
 getHousings = async (req, res) => {
     try {
-        const housings = await prisma.housings.findMany()
+        const housings = await prisma.Housings.findMany()
         res.json(housings)
     } catch (err) {
-        console.log("Error getHousings")
-    } }
+        console.error("Error getHousings : " + err)
+         res.status(500).json({ error: "Internal server error" })
+} }
 
- module.exports = {getHousings}
+getHousingById = async (req, res) => {
+    const {id} = req.params
+    console.log(id)
+    try {
+        const housings = await prisma.Housings.findMany(
+            {where:{id: id},}
+        )
+        res.json(housings)
+    } catch (err) {
+        console.error("Error getHousings : " + err)
+         res.status(500).json({ error: "Internal server error" })
+} }
+
+ module.exports = {getHousings,getHousingById}
