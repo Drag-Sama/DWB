08e97171bde54c1271a93baa22b2fdd38de545c5
Anto Kajda <antoinekajda@gmail.com>
2026-03-31 11:26:31 +0200

    WIP Error management

--- Middleware/HousingMiddleware.js
+++ Middleware/HousingMiddleware.js
@@ -1,15 +1,36 @@
 const { body , validationResult} = require('express-validator');
-const housingController = require("../Controller/HousingController")
 
-const isValueValid = () => body('city', 'City must not be empty').notEmpty();
+const isValuesValid = () => [
+    body('adresse').notEmpty().withMessage('Adresse must not be empty')
+        .isString().withMessage('Adresse must be a string'), 
 
-addHousing = async(req, res) => {
-    const errors = validationResult(req);
-    if(errors.isEmpty())
-        housingController.addHousing(req,res)
-    else{
-        res.status(400).json({ error: "Internal server errorss" })
-    }
-}
+    body('city').notEmpty().withMessage('City must not be empty')
+        .isString().withMessage('City must be a string'), 
+    
+    body('name').notEmpty().withMessage('Name must not be empty')
+        .isString().withMessage('Name must be a string'), 
 
-module.exports = {isValueValid, addHousing}
+    body('price').notEmpty().withMessage('Price must not be empty')
+        .isInt().withMessage('Price must be an int'),   
+    
+    body('size').notEmpty().withMessage('Size must not be empty')
+        .isInt().withMessage('Size must be an int'),   
+
+    body('ownerId').notEmpty().withMessage('OwnerId must not be empty')
+        .isString().withMessage('OwnerId must be a string'),     
+];
+
+const validate = (validations) => {
+    return async (req, res, next) => {
+        await Promise.all(validations.map(v => v.run(req)));
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        next();
+    };
+};
+
+const validateAdd = validate(isValuesValid());
+
+module.exports = {validateAdd}
--- Routes/housing.js
+++ Routes/housing.js
@@ -8,24 +8,11 @@ const router = express.Router();
 router.get("/", housingController.getHousings)
 router.get("/get", housingController.getHousingsByProperty)
 router.get("/sort/:property", housingController.sortHousingsByProperty)
-router.get("/filtermin/:property/:value", housingController.sortHousingsByProperty)
-router.get("/filtermax/:property/:value", housingController.sortHousingsByProperty)
 //POST
-router.post("/",  housingMiddleware.isValueValid(), housingMiddleware.addHousing)
+router.post("/", housingMiddleware.validateAdd, housingController.addHousing)
 //DELETE
 router.delete("/:id", housingController.deleteHousings)
 //PUT
 router.put("/:id", housingController.updateHousing)
-/*
-router.get("/:id", (req,res) => res.send(housingController.fetchHousingById(req.params.id)))
-router.get("/filter/:property/:value", (req,res) => res.send(housingController.fetchHousingBy(req.params.property,req.params.value)))
-router.get("/sort/:property", (req,res) => res.send(housingController.fetchHousingSortedBy(req.params.property)))
 
-//Post
-router.post("/:housing", (req,res) => res.send(housingController.createHousing(req.params.housing)))
-//Delete
-router.delete("/:id", (req, res) => res.send(housingController.deleteHousing(req.params.id)))
-//Put
-router.put("/:id/:housing", (req, res) => res.send(housingController.editHousing(req.params.housing, req.params.id)))
-*/
 module.exports = router;
